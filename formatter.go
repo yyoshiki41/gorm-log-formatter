c1428a5c4a19649059aa9b93e14320d427b343fb
yyoshiki41 <yyoshiki41@gmail.com>
2021-09-08 00:10:48 +0900

    refactored

--- formatter.go
+++ formatter.go
@@ -1,3 +1,4 @@
+// This file was taken from the jinzhu/gorm
 package formatter
 
 import (
@@ -24,18 +25,24 @@ func isPrintable(s string) bool {
 	return true
 }
 
+// gorm.LogFormatter compatible parser.
+// returend values are below.
+//   []interface{}{source, currentTime, duration, rawSQL, rows}.
 var LogFormatter = func(values ...interface{}) (messages []interface{}) {
 	if len(values) > 1 {
 		var (
 			sql             string
 			formattedValues []string
 			level           = values[0]
+			currentTime     = NowFunc().Format("2006-01-02 15:04:05")
 			source          = fmt.Sprintf("(%v)", values[1])
 		)
 
-		messages = []interface{}{source}
+		messages = []interface{}{source, currentTime}
 
 		if len(values) == 2 {
+			//remove the line break
+			currentTime = currentTime[1:]
 			//remove the brackets
 			source = fmt.Sprintf("%v", values[1])
 
@@ -43,6 +50,9 @@ var LogFormatter = func(values ...interface{}) (messages []interface{}) {
 		}
 
 		if level == "sql" {
+			// duration
+			messages = append(messages, fmt.Sprintf("%.2f", float64(values[2].(time.Duration).Nanoseconds()/1e4)/100.0))
+
 			// sql
 			for _, value := range values[4].([]interface{}) {
 				indirectValue := reflect.Indirect(reflect.ValueOf(value))
@@ -96,14 +106,17 @@ var LogFormatter = func(values ...interface{}) (messages []interface{}) {
 				}
 			}
 			messages = append(messages, sql)
-			// duration
-			messages = append(messages, fmt.Sprintf(" [%.2fms] ", float64(values[2].(time.Duration).Nanoseconds()/1e4)/100.0))
+
 			// rows
-			messages = append(messages, fmt.Sprintf(" \n[%v] ", strconv.FormatInt(values[5].(int64), 10)+" rows affected or returned"))
+			messages = append(messages, fmt.Sprintf("%v", strconv.FormatInt(values[5].(int64), 10)))
 		} else {
 			messages = append(messages, values[2:]...)
 		}
 	}
 
 	return
+}
+
+var NowFunc = func() time.Time {
+	return time.Now()
 }
